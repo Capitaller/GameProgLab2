f2825469851421fb1a6c59e2f8c557d1b00fd648
Capitaller <130102anton@gmail.com>
2022-09-18 20:26:12 +0300

    Zvit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -79,11 +79,6 @@ public class PlayerMovement : MonoBehaviour
             sc.NextLevel("Scene2");
 
         }
-        //if (Coll.gameObject.tag == "Coin")
-        //{
-        //    sm.ScoreCount();
-        //    Destroy(gameObject);
 
-        //}
     }
 }
